95f33d8e69fe5e15eed2996c04213203e4efbbdf
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-14 13:42:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,6 +14,19 @@ class Triangle
     if a == b && b == c
       :equilateral
     elsif a == b || b == c || a == c
-      :isosceles 
+      :isosceles
+    else
+      :scalene
+    end 
+end
+
+def validate_triangle 
+  real_triangle = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each { |s| real_triangle << false if s <= 0 }
+    raise TriangleError if real_triangle.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
 
 end
